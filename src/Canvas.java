4c4b14bcf4fa09cdb3920993cf4005816e3421e8
RemiFreret <remi.freret@hotmail.fr>
2017-02-17 15:54:58 +0100

    Mouvement du pacman débloqué

--- src/Canvas.java
+++ src/Canvas.java
@@ -276,21 +276,44 @@ public class Canvas
         @Override
         public void keyPressed(KeyEvent event) {
             switch (event.getKeyCode()) {
-	    case KeyEvent.VK_UP: upPressed = true; break;
-	    case KeyEvent.VK_DOWN: downPressed = true; break;
-	    case KeyEvent.VK_LEFT: leftPressed = true; break;
-	    case KeyEvent.VK_RIGHT: rightPressed = true; break;
+        	    case KeyEvent.VK_UP:
+                upPressed = true;
+                downPressed = false;
+                leftPressed = false;
+                rightPressed = false;
+                break;
+        	    case KeyEvent.VK_DOWN:
+                upPressed = false;
+                downPressed = true;
+                leftPressed = false;
+                rightPressed = false;
+                break;
+        	    case KeyEvent.VK_LEFT:
+                upPressed = false;
+                downPressed = false;
+                leftPressed = true;
+                rightPressed = false;
+                break;
+        	    case KeyEvent.VK_RIGHT:
+                upPressed = false;
+                downPressed = false;
+                leftPressed = false;
+                rightPressed = true;
+                break;
             }
         }
 
         @Override
         public void keyReleased(KeyEvent event) {
+          /*
             switch (event.getKeyCode()) {
 	    case KeyEvent.VK_UP: upPressed = false; break;
 	    case KeyEvent.VK_DOWN: downPressed = false; break;
 	    case KeyEvent.VK_LEFT: leftPressed = false; break;
 	    case KeyEvent.VK_RIGHT: rightPressed = false; break;
+
             }
+            */
         }
     }
 }
